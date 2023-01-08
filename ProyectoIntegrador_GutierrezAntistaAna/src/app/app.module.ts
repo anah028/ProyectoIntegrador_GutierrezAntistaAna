b89414eb521a28de4f6bf825c21272f705b8989c
anah028 <116830196+anah028@users.noreply.github.com>
2023-01-08 17:39:01 -0300

    Header

--- ProyectoIntegrador_GutierrezAntistaAna/src/app/app.module.ts
+++ ProyectoIntegrador_GutierrezAntistaAna/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgPComponent } from './components/logo-arg-p/logo-arg-p.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoArgPComponent
+    LogoArgPComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
