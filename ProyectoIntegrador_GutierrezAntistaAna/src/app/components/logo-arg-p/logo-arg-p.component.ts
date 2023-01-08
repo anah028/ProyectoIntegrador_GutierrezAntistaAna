a7d5420803dbf2861cbc7ad35bb4ebc4c2a93d2e
anah028 <116830196+anah028@users.noreply.github.com>
2023-01-08 16:34:12 -0300

    agregamos imagenes de logos y usamos angular

---
+++ ProyectoIntegrador_GutierrezAntistaAna/src/app/components/logo-arg-p/logo-arg-p.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-arg-p',
+  templateUrl: './logo-arg-p.component.html',
+  styleUrls: ['./logo-arg-p.component.css']
+})
+export class LogoArgPComponent {
+
+}
